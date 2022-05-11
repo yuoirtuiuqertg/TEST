e4dba32a342bb2059a215970e6ed1144cc5cb1b8
Ivan <mr.mr.van@gmail.com>
2022-05-11 04:34:41 +0300

    Исправлены недочеты

--- .idea/src/com/company/Сin.java
+++ .idea/src/com/company/Сin.java
@@ -3,6 +3,11 @@ package com.company;
 import java.util.Scanner;
 
 public class Сin { // ввожу пример
+    private String operation;
+    public String getOperation(){
+        operation = numbers();
+        return operation;
+    }
     private String numbers() {
         Scanner in = new Scanner(System.in);
         String number = in.nextLine();
