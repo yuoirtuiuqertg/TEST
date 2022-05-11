b04ee726c3b774cca960bb722e5ef573d40c50af
Ivan <mr.mr.van@gmail.com>
2022-05-11 04:19:05 +0300

    Исправлены недочеты

--- .idea/src/com/company/Main.java
+++ .idea/src/com/company/Main.java
@@ -3,8 +3,22 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        Calc one = new Calc();
-        one.calc();
-        return;
+        GetNumberAndOperator arrOperation = new GetNumberAndOperator();
+        Romain numberRomain = new Romain();
+        Arabian numberArabian = new Arabian();
+        MathOperator romainOrArabian = new MathOperator();
+
+        try{
+            String[] temp = arrOperation.arrOperatorNums();
+            if(romainOrArabian .ArabinOrRomane(temp)== true) {
+                System.out.print(numberRomain.otvet(temp));
+            } else {
+                int tem = numberArabian.CalculaterArabian(numberArabian.numReem(temp[0]),numberArabian.numReem(temp[2]) , temp[1]);
+                System.out.println(numberArabian.transformNumberRomanNumeral(tem));
+            }
+        } catch (Exp e){
+            System.out.println("Текст Ошибки : " + e.getMessage());
+            System.out.println("Код ошибки : " + e.getNumber());
+        }
     }
 }
